d82614b3e4419525b26f8a5782052478e32f66c4
Borja De Madariaga <bde-mada@student.42urduliz.com>
2023-02-24 09:50:04 +0100

    ex06 part2

--- Python-Bootcamp/Module00/ex06/recipe.py
+++ Python-Bootcamp/Module00/ex06/recipe.py
@@ -6,16 +6,26 @@
 #    By: bde-mada <bde-mada@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/02/23 18:25:53 by bde-mada          #+#    #+#              #
-#    Updated: 2023/02/23 23:53:10 by bde-mada         ###   ########.fr        #
+#    Updated: 2023/02/24 09:47:39 by bde-mada         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import sys
 
 def print_recipe_names(cookbook):
-    i = 1
+    for pos in range(len(list(cookbook.keys()))):
+        print(f"Recipe {pos + 1}: {list(cookbook.keys())[pos]}")
 
 
+def print_recipe_details(cookbook, recipe):
+    for pos in range(len(list(cookbook.keys()))):
+        if list(cookbook.keys())[pos] == recipe:
+            print(f'Ingredients:')
+            print(f'Meal: {cookbook[recipe]["meal"]}')
+            print(f'Meal: {cookbook[recipe]["prep_time"]} min')
+
+
+    
 def main():
     cookbook = {'Sandwich' : dict(ingredients = ['ham', 'bread', 'cheese', 'tomatoes'], meal = 'lunch', prep_time = 10),
                     'Cake' : dict(ingredients = ['flour', 'sugar', 'eggs'], meal = 'dessert', prep_time = 60),
@@ -24,6 +34,9 @@ def main():
     print("List of the available options:")
     print("   1: Add a recipe\n   2: Delete a recipe\n   3: Print a recipe")
     print("   4: Print the cookbook\n   5: Quit\n\nPlease select an option:")
+    print(cookbook)
+    print_recipe_names(cookbook)
+    print_recipe_details(cookbook, 'Sandwich')
 
 
 if __name__ == '__main__':
